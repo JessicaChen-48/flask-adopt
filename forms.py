b48dacbe3af9efda6de8764975bc53ad3d85f78f
Christina Schweipert <c.schweipert@gmail.com>
2021-03-12 13:01:27 -0700

    create form model and add validation route.

--- forms.py
+++ forms.py
@@ -1,3 +1,33 @@
-
-
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtfforms import Stringfield, SelectField, TextAreaField, BooleanField
+from wtf.validators import InputRequired, Length, Optional
+
+class AddPet(FlaskForm):
+    """Adds a pet"""
+
+    name = Stringfield(
+        "Name",
+        validators=[InputRequired()],
+    )
+
+    species = SelectField(
+        "Species",
+        choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")],
+    )
+
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    ) 
+
+    age = SelectField(
+        "Age",
+        choices=[("baby", "Baby"), ("young", "Young"), ("adult", "Adult"), ("senior", "Senior")],
+    )
+
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional()]
+    )
